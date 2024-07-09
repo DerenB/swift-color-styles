4e12d863d729baf28556d7fa9bdfe922da68c094
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-08 20:14:12 -0400

    Testing Components

--- AppColorStyles/AppColorStyles/Screens/ColorsVC.swift
+++ AppColorStyles/AppColorStyles/Screens/ColorsVC.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class ColorsVC: UIViewController {
+    
+    let testView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemGray
+        
+        configureView()
+    }
+    
+    
+    func configureView() {
+        view.addSubview(testView)
+        testView.translatesAutoresizingMaskIntoConstraints = false
+        
+        testView.backgroundColor = .systemRed
+        
+        NSLayoutConstraint.activate([
+            testView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            testView.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
 
+}
+
+
+#Preview {
+    ColorsVC()
 }
