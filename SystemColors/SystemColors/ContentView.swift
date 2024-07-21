5c72b8748cd2752db1f06171af1df52337fdadfb
DerenB <dbozerprogramming@gmail.com>
2024-07-21 15:51:21 -0400

    Project Creation

---
+++ SystemColors/SystemColors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SystemColors
+//
+//  Created by Deren Bozer on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SystemColors/SystemColors/SystemColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemColorsApp.swift
+//  SystemColors
+//
+//  Created by Deren Bozer on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SystemColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
